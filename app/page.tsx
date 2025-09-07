a64a06138904f4375d3bb2a35f0c694fe4da5212
Godfrey <godfrey@aprilpark.io>
2025-09-07 14:57:11 -0500

    fix lint error

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,13 @@ declare global {
   interface Window {
     voiceflow?: {
       chat: {
-        load: (config: any) => void;
+        load: (config: {
+          verify: { projectID: string };
+          url: string;
+          versionID: string;
+          voice?: { url: string };
+          render?: { mode: string; target: HTMLElement | null };
+        }) => void;
       };
     };
   }
